c2c19a9047a7961b53ea4b8181c096b8c6d5354e
whxway <whxway@qq.com>
2020-09-20 17:17:11 +0800

    format code

--- uCOSII/Include/ucosinc.h
+++ uCOSII/Include/ucosinc.h
@@ -1,12 +1,15 @@
 #ifndef __UCOSINC_H_
 #define __UCOSINC_H_
-#include <stdio.h>
-#include <string.h>
+
 #include <ctype.h>
-#include <stdlib.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "os.h"
-#include "os_cpu.h"
 #include "os_cfg.h"
+#include "os_cpu.h"
 #include "stm32f4xx.h"
+
 #endif
